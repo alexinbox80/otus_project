d1f5fcf7d43d618373ae0dc5dabe99afc7820eb6
AlexeySerov <alexinbox80@inbox.ru>
2025-02-26 12:41:40 +0300

    Process signals via EventSubscriber

--- src/Controller/Cli/AddFollowersCommand.php
+++ src/Controller/Cli/AddFollowersCommand.php
@@ -6,7 +6,6 @@ use App\Domain\Service\FollowerService;
 use App\Domain\Service\UserService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Command\SignalableCommandInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -14,7 +13,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 
 #[AsCommand(name: self::FOLLOWERS_ADD_COMMAND_NAME, description: 'Add followers to author', hidden: true)]
-final class AddFollowersCommand extends Command implements SignalableCommandInterface
+final class AddFollowersCommand extends Command
 {
     public const FOLLOWERS_ADD_COMMAND_NAME = 'followers:add';
 
